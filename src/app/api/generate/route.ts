151c4e2e4da012a7985d29709dc0e09b6932e2d7
xodn_ghkd <hwa1023@mjc.ac.kr>
2025-02-27 19:10:25 +0900

    gemini api 연동 및 답변 md -> html로 convert logic 추가

---
+++ src/app/api/generate/route.ts
@@ -0,0 +1,22 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest, res: NextResponse) {
+  try {
+    const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
+
+    const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
+
+    const data = await req.json();
+
+    const prompt = data.body;
+
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const output = await response.text();
+
+    return NextResponse.json({ output });
+  } catch (err) {
+    console.error(err);
+  }
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,71 @@
-export default function Home() {
-  return (
-    <div>
+"use client"
 
+import { ReactElement, useState } from "react";
+import { unified } from "unified";
+import markdown from "remark-parse";
+import remarkRehype from "remark-rehype";
+import html from "rehype-stringify";
+
+export default function Home() {
+
+  // const prompt = "한국어로 인사해봐";
+  const [prompt, setPrompt] = useState('');
+  const [output, setOutPut] = useState('');
+  const [test, setTest] = useState<ReactElement>();
+
+
+  const generateText = async () => {
+    try {
+      const response = await fetch("/api/generate", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json"
+        },
+        body: JSON.stringify(({ body: prompt }))
+      })
+      const data = await response.json();
+
+      if (response.ok) {
+        console.log(convertHTML(data.output))
+        setOutPut(data.output);
+      } else {
+        setOutPut(data.error);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  const convertHTML = (md: string) => {
+    try {
+      const html_text = unified()
+        .use(markdown)
+        .use(remarkRehype)
+        .use(html)
+        .processSync(md);
+
+      setTest(<div dangerouslySetInnerHTML={{ __html: html_text.value }} />);
+
+      return html_text.value;
+    }
+    catch (err) {
+      console.error("Markdown parsing error: ", err);
+      setTest(<div dangerouslySetInnerHTML={{ __html: md }} />);
+    }
+  }
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.currentTarget;
+
+    setPrompt(value);
+  }
+
+  return (
+    <div className="flex flex-col items-center">
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input onChange={onChange} className="bg-black" value={prompt} />
+        <button onClick={generateText}>눌러줘요</button>
+      </form>
+      {test}
     </div>
   );
 }
